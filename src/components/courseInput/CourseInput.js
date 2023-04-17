a25977949ef8385b98f4d2c673883b3c3244c835
Bryant Richards <dragonseyeimages@gmail.com>
2023-04-17 07:32:04 -0500

    adds dynamic styling

--- src/components/courseInput/CourseInput.js
+++ src/components/courseInput/CourseInput.js
@@ -5,21 +5,33 @@ import './style.css'
 
 const CourseInput = ({ onAddGoal }) => {
   const [enteredValue, setEnteredValue] = useState('')
+  const [isValid, setIsValid] = useState(true)
 
   const goalInputChangeHandler = (event) => {
     setEnteredValue(event.target.value)
+    if (enteredValue.trim().length > 0) {
+      setIsValid(true)
+    }
   }
 
   const formSubmitHandler = (event) => {
     event.preventDefault()
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false)
+      return
+    }
+    setIsValid(true)
     onAddGoal(enteredValue)
   }
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className='form-control'>
+      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
         <label>Course Goal</label>
-        <input type='text' onChange={goalInputChangeHandler} />
+        <input
+        type='text' 
+        onChange={goalInputChangeHandler}
+         />
       </div>
       <Button type='submit'>Add Goal</Button>
     </form>
